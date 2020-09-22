476f6425ecb84865d2d38dc59c724bcb81e96dc5
“jatinarora30” <arorajatinkumar2000@gmail.com>
2020-09-22 14:07:02 +0530

    Game:Catch me if u can

---
+++ project1_ws/nodes/tf_listener4.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python  
+import roslib
+roslib.load_manifest('project1_ws')
+import rospy
+import math
+import tf
+import geometry_msgs.msg
+import turtlesim.srv
+
+if __name__ == '__main__':
+    rospy.init_node('tf_listener4')
+
+    listener = tf.TransformListener()
+
+    rospy.wait_for_service('spawn')
+    spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
+    spawner(4,1, 0, 'turtle4')
+    turtle_vel = rospy.Publisher('turtle4/cmd_vel', geometry_msgs.msg.Twist,queue_size=1)
+
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        try:
+            (trans,rot) = listener.lookupTransform('/turtle4', '/turtle1', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+        if not (math.sqrt(trans[0] ** 2 + trans[1] ** 2))<0.1:
+            angular = 0.7 * math.atan2(trans[1], trans[0])
+            linear = 0.1 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+            cmd = geometry_msgs.msg.Twist()
+            cmd.linear.x = linear
+            cmd.angular.z = angular
+            turtle_vel.publish(cmd)
+        else:
+            print("Game over")
+            rospy.signal_shutdown()
+
+        rate.sleep()
